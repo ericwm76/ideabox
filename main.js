ab18b73ee6a44912ea4b4f2d995b8eeda3590c09
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-07-24 10:25:34 -0600

    Align the main.js with the Turing style guide
    Co-authored-by Eric Meldrum <eric.meldrum@gmail.com>
    Co-authored-by Colin Koga <colin.koga@gmail.com>

--- main.js
+++ main.js
@@ -22,12 +22,12 @@ showTenOnLoad();
 injectIntro(); 
 
 function showTenOnLoad() {
-  if (ideasArray.length > 10){
+  if (ideasArray.length > 10) {
     var recentTenIdeas = [];
-    for (var i = 0; i < 10; i++){
+    for (var i = 0; i < 10; i++) {
       recentTenIdeas.unshift(ideasArray[ideasArray.length - 1 -i])
     }; 
-    clearIdeaBoard()
+    clearIdeaBoard();
     recentTenIdeas.forEach(function(idea) {
     displayIdea(idea);
     });
@@ -47,16 +47,19 @@ function navEventHandler(e) {
   if (e.target.id === 'more-less-btn') {
     showMoreLess(e);
   };
+
   if (e.target.closest('#show-star-btn')) {
     toggleStarBtn(e);
   };
+
   if (e.target.closest('.js-switch')) {
     toggleQualities(e);
-  }
+  };
+
   if (e.target.closest('#menu-button')) {
     toggleBurger(e);
-  }  
-}
+  };  
+};
 
 function updateArticle(e) {
   e.preventDefault();
@@ -82,37 +85,6 @@ function disableSave() {
   };
 };
 
-function displayIdea(ideaObj) {
-  var qualityText = [
-    "Quality: Swill",
-    "Quality: Plausible",
-    "Quality: Genius"
-  ];
-  if (ideaObj.star === true) {
-    var star = 'images/star-active.svg';
-  } else {
-    var star = 'images/star.svg';
-  };
-
-  ideaBoard.insertAdjacentHTML(
-    "afterbegin",`<article class="article" data-identifier="${ideaObj.id}">
-          <header class="article__header">
-            <img src="${star}" id="star-img" alt="picture of a star white">
-            <img src="images/delete.svg" id="delete-x" alt="white x">
-          </header>
-          <div>
-            <p contenteditable= "true" class="article__title">${ideaObj.title}</p>
-            <p contenteditable= "true" class="article__body">${ideaObj.body}</p>
-            <footer class="article__footer">
-              <img src="images/upvote.svg" id="up-arrow" alt="arrow pointing up white">
-              <p class="article__quality" id="idea-quality">${qualityText[ideaObj.quality]}</span></p>
-              <img src="images/downvote.svg" id="down-arrow" alt="arrow pointing down white">
-          </div>
-          </footer>
-      </article>`
-  );
-};
-
 function clearInputs() {
   titleInput.value = '';
   bodyInput.value = '';
@@ -120,6 +92,7 @@ function clearInputs() {
 
 function createObj() {
   var newIdea = new Idea({title: titleInput.value, body: bodyInput.value, star: false, quality: 0, id: Date.now()});
+
   ideasArray.push(newIdea);
   newIdea.saveToStorage(ideasArray);
   sortIdeas();
@@ -134,18 +107,18 @@ function persistOnLoad() {
  };
 
 function toggleQualities(e) {
-  console.log('hi')
-  var nodesIndexList = document.querySelectorAll('.js-switch')
+  var nodesIndexList = document.querySelectorAll('.js-switch');
+
   nodesIndexList.forEach(function(index) {
     index.classList.add('swill-quality');
     index.classList.remove('swill-quality-active');
   });
+
   e.target.closest('.js-switch').classList.add('swill-quality-active'); 
   clearIdeaBoard();
-  console.log(e.target.dataset.quality);
+
   qualitiesArray[parseInt(e.target.dataset.quality)].forEach(function(ideaObj) {
     displayIdea(ideaObj);
-    console.log('bye')
   });
 };
 
@@ -184,19 +157,19 @@ function saveCard(e) {
   };
 };
 
-function changeQuality(e){
+function changeQuality(e) {
   if (e.target.id === 'up-arrow' && ideasArray[getIndex(e)].quality < qualitiesArray.length - 1) {
     ideasArray[getIndex(e)].quality++;
     ideasArray[getIndex(e)].saveToStorage(ideasArray);  
     sortIdeas();
     changeQualityText(e);
-  } else if (e.target.id === 'down-arrow' && ideasArray[getIndex(e)].quality > 0){
+  } else if (e.target.id === 'down-arrow' && ideasArray[getIndex(e)].quality > 0) {
     ideasArray[getIndex(e)].quality--
     ideasArray[getIndex(e)].saveToStorage(ideasArray);
     sortIdeas();
     changeQualityText(e);
-  }
-}
+  };
+};
 
 function changeQualityText(e) {
   var qualityText = [
@@ -204,6 +177,7 @@ function changeQualityText(e) {
     'Quality: Plausible',
     'Quality: Genius'
   ];
+
   e.target.parentNode.childNodes[3].innerText = qualityText[ideasArray[getIndex(e)].quality];
 };
 
@@ -213,7 +187,7 @@ function getIdentifier(e) {
 
 function getIndex(e) {
   return ideasArray.findIndex(function(id) {
-    return parseInt(getIdentifier(e)) === id.id
+    return parseInt(getIdentifier(e)) === id.id;
   });
 };
 
@@ -228,6 +202,7 @@ function findIdeaToRemove(e) {
 function favoriteIdea(e) {
   var activeStar = 'images/star-active.svg';
   var inactiveStar = 'images/star.svg';
+
   if (ideasArray[getIndex(e)].star === true) {
     e.target.src = activeStar;
   } else {
@@ -246,38 +221,43 @@ function saveStar(e) {
 
 function filterBySearch() { 
   var totalArray = qualitiesArray.concat([ideasArray]);
-  console.log(totalArray);
-  var searchedArray = totalArray[getActiveFilter()].filter(function(idObj) {
+  var searchedArray = [];
+
+  searchedArray = totalArray[getActiveFilter()].filter(function(idObj) {
     return idObj.body.toLowerCase().includes(document.querySelector('#search-input').value.toLowerCase()) 
      || idObj.title.toLowerCase().includes(document.querySelector('#search-input').value.toLowerCase());
   });
+
   if (showStarBtn.classList.contains('active')) {
     searchedArray = compareArray(searchedArray, displayStarredIdeas());
-  }
+  };
+
   return searchedArray;
 };
 
 function compareArray(array1, array2) {
   var searchArray = [];
+
   array1.forEach(function(ideaObj) {
     if (array2.includes(ideaObj)) {
       searchArray.push(ideaObj);
-      // displayIdea(ideaObj);
-    }
-  })
-  return searchArray
-}
+    };
+  });
+
+  return searchArray;
+};
 
 function getActiveFilter() {
   var activeFilter = 3;
+
   document.querySelectorAll('.js-switch').forEach(function(btn) {
     if (btn.classList.contains('swill-quality-active')) {
       activeFilter = parseInt(btn.dataset.quality);
     };
   });
-  return activeFilter;
-}
 
+  return activeFilter;
+};
 
 function clearIdeaBoard() {
   ideaBoard.innerHTML = '';
@@ -286,6 +266,7 @@ function clearIdeaBoard() {
 function repopulateMain() {
   clearIdeaBoard();
   filterBySearch().forEach(element => displayIdea(element));
+
   if (document.querySelector('#search-input').value === '') {
     clearIdeaBoard();
     var totalArray = qualitiesArray.concat([ideasArray]); 
@@ -295,28 +276,29 @@ function repopulateMain() {
   };
 };
 
-
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
 function displayStarredIdeas() {
   var favIdeas = [];
-    ideasArray.filter(function(ideaObj) {
-      if (ideaObj.star === true) {
-        favIdeas.push(ideaObj)
-      };
-    });
-      if (favIdeas.length === 0) {
-        ideaBoard.insertAdjacentHTML("afterbegin", `<card id="js-no-fave-msg"><p>You have no starred ideas! Click the star on the card to favorite your ideas!</p></card>`);
-        showStarBtn.classList.remove('active');
-        showStarBtn.innerText = 'Show Starred Ideas';
-        return favIdeas;
-      };
-        clearIdeaBoard();
-      favIdeas.forEach(function(ideaObj) {
-        displayIdea(ideaObj);
-        });
-      return favIdeas;
+
+  ideasArray.filter(function(ideaObj) {
+    if (ideaObj.star === true) {
+      favIdeas.push(ideaObj)
+    };
+  });
+
+  if (favIdeas.length === 0) {
+    insertFavPrompt();
+    showStarBtn.classList.remove('active');
+    showStarBtn.innerText = 'Show Starred Ideas';
+    return favIdeas;
   };
+
+  clearIdeaBoard();
+  favIdeas.forEach(function(ideaObj) {
+    displayIdea(ideaObj);
+  });
+
+  return favIdeas;
+};
 
 function toggleStarBtn() {
   if (showStarBtn.classList.contains('active')) {
@@ -332,44 +314,35 @@ function toggleStarBtn() {
 };
 
 function sortIdeas() {
-  qualitiesArray = [[], [], []]
+  qualitiesArray = [[], [], []];
+
   ideasArray.forEach(function(ideaObj) {
     qualitiesArray[ideaObj.quality].push(ideaObj)
   });
 };
 
-
-
-function injectIntro() {
-  if (ideaBoard.innerHTML === '' || ideaBoard.innerHTML === ' '){
-   clearIdeaBoard()
-   ideaBoard.insertAdjacentHTML("afterbegin", 
-    ` <card id="js-card">
-        <p>Add your wonderful ideas.  Fill out the form and click "Save"</p>
-      </card>`)
-  }; 
-};
-
 function removeIntro() {
   var element = document.getElementById('js-card');
+
   if (element) {
-  element.parentNode.removeChild(element);
+    element.parentNode.removeChild(element);
   };  
 };
 
-//need to refactor so opposite functions.
-
 function showMoreLess(e) { 
+  var lessIdeas = [];
+
   if (e.target.classList.contains('nav__button') && e.target.classList.contains('active')) {
     e.target.classList.remove('active');
-    var lessIdeas = [];
-    for (var i = 0; i < 10; i++){
+    for (var i = 0; i < 10; i++) {
       lessIdeas.unshift(ideasArray[ideasArray.length - 1 -i]);
     }; 
-    clearIdeaBoard()
+
+    clearIdeaBoard();
     lessIdeas.forEach(function(ideaObj) {
-    displayIdea(ideaObj);
+      displayIdea(ideaObj);
     });
+
     e.target.innerHTML = 'Show More';
   } else {
     e.target.classList.add('active');
@@ -377,4 +350,55 @@ function showMoreLess(e) {
     clearIdeaBoard();
     persistOnLoad();
   };
+};
+
+function displayIdea(ideaObj) {
+  var star = 'images/star.svg';
+  var qualityText = [
+    "Quality: Swill",
+    "Quality: Plausible",
+    "Quality: Genius"
+  ];
+
+  if (ideaObj.star === true) {
+    star = 'images/star-active.svg';
+  } else {
+    star = 'images/star.svg';
+  };
+
+  ideaBoard.insertAdjacentHTML(
+    "afterbegin",`<article class="article" data-identifier="${ideaObj.id}">
+          <header class="article__header">
+            <img src="${star}" id="star-img" alt="picture of a star white">
+            <img src="images/delete.svg" id="delete-x" alt="white x">
+          </header>
+          <div>
+            <p contenteditable= "true" class="article__title">${ideaObj.title}</p>
+            <p contenteditable= "true" class="article__body">${ideaObj.body}</p>
+            <footer class="article__footer">
+              <img src="images/upvote.svg" onmouseover="this.src='images/upvote-active.svg'" onmouseout="this.src='images/upvote.svg'" id="up-arrow" alt="arrow pointing up white">
+              <p class="article__quality" id="idea-quality">${qualityText[ideaObj.quality]}</span></p>
+              <img src="images/downvote.svg" onmouseover="this.src='images/downvote-active.svg'" onmouseout="this.src='images/downvote.svg'"id="down-arrow" alt="arrow pointing down white">
+          </div>
+          </footer>
+      </article>`
+  );
+};
+
+function injectIntro() {
+  if (ideaBoard.innerHTML === '' || ideaBoard.innerHTML === ' ') {
+   clearIdeaBoard();
+   ideaBoard.insertAdjacentHTML("afterbegin", 
+    `<card id="js-card">
+      <p>Add your wonderful ideas.  Fill out the form and click "Save"</p>
+    </card>`)
+  }; 
+};
+
+function insertFavPrompt() {
+  ideaBoard.insertAdjacentHTML("afterbegin",
+    `<card id="js-no-fave-msg">
+      <p>You have no starred ideas! Click the star on the card to favorite your ideas!</p>
+    </card>`
+  );
 };
